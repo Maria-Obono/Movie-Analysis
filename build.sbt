6943970f9306d227c8d40642b9a8f0b899405826
Maria Gloria Raquel Obono <obonoondo.m@northeastern.edu>
2023-11-30 01:47:33 -0500

    los

--- build.sbt
+++ build.sbt
@@ -6,3 +6,20 @@ lazy val root = (project in file("."))
   .settings(
     name := "Movie-Analyzer"
   )
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.5.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "3.5.0" % "provided"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib-local
+libraryDependencies += "org.apache.spark" %% "spark-mllib-local" % "3.5.0"
+
+// https://mvnrepository.com/artifact/org.scalatest/scalatest
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.3.0-SNAP4" % Test
+
+
---
+++ src/main/scala/MovieratingDataset.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{mean, stddev}
+
+object MovieratingDataset {
+  def main(args: Array[String]): Unit = {
+    // Create a SparkSession
+    val spark = SparkSession.builder()
+      .appName("MovieratingDataset")
+      .master("local[*]") // Change this to your cluster setup
+      .getOrCreate()
+
+    // Read the CSV file into a DataFrame
+    val filePath = "/Users/mariagloriaraquelobono/Fall2023/Movie-Analyzer/src/main/resources/ratings.csv"
+    val movieData = spark.read.option("header", "true").csv(filePath)
+
+    // Calculate mean rating and standard deviation
+    val meanRating = BigDecimal(movieData.select(mean("rating")).first().getDouble(0)).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val stdDevRating = BigDecimal(movieData.select(stddev("rating")).first().getDouble(0)).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+    println(s"Mean Rating: $meanRating")
+    println(s"Standard Deviation of Rating: $stdDevRating")
+
+    // Stop the SparkSession
+    spark.stop()
+  }
+
+}
